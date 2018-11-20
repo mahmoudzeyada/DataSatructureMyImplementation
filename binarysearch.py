559def8b4240cc816a300eadee524866418f8780
Mahmoud Zeyada <mahmoudzeyada@192.168.1.6>
2018-11-21 01:25:41 +0200

    binaryserach

---
+++ binarysearch.py
@@ -0,0 +1,24 @@
+def binary_search(input_array,value):
+    input_array.sort()
+    #import pdb; pdb.set_trace()
+    pointer=0
+    max=len(input_array)-1
+    min=0
+    while min <= max:
+        pointer=(max+min)/2
+        if input_array[pointer]==value:
+            return pointer
+        elif input_array[pointer] < value :
+            min=pointer+1
+        elif input_array[pointer] > value:
+            max = pointer-1
+
+    return -1
+
+
+
+test_list = [1,3,9,11,15,19,30]
+test_val1 =3
+#test_val2 =3
+print binary_search(test_list, test_val1)
+#print binary_search(test_list, test_val2)
