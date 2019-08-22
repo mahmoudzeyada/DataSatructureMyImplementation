bbd46cb57219b27903ddeda9791853cfb833d8eb
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-08-22 02:41:58 +0200

    impleminting binary heap tree

--- heapbinarytree.py
+++ heapbinarytree.py
@@ -1,5 +1,5 @@
 def parent(i):
-    return i/2
+    return int((i-1)/2)
 
 
 def left(i):
@@ -11,7 +11,7 @@ def right(i):
 
 
 def heapify(a, i):
-    ''' function for mantaing max heap property  '''
+    ''' function for mantaing max heap property time-complexity = O(log n) but that it is that is optimized version  '''
     l = left(i)
     r = right(i)
 
@@ -30,10 +30,56 @@ def heapify(a, i):
 
 
 def build_max_heap(a):
-    ''' function for building max heap binary tree '''
+    ''' function for building max heap binary tree time-complexity = O(n) '''
     for i in range(int(len(a)/2)):
         heapify(a, i)
     return a
 
 
-print(build_max_heap([3, 9, 8, 2, 2, 7]))
+def heap_extract_max(a):
+    ''' function for extracting max  time-complexity = O(n log(n)) '''
+    heap = build_max_heap(a)
+    for _ in range(len(heap)):
+        max_item = heap[0]
+        print(max_item)
+        heap[0], heap[-1] = heap[-1], heap[0]
+        heap = heapify(heap[:-1], 0)
+
+
+def heap_increase_key(heap, index, key):
+    ''' function for increasing key time-complexity = O(log(n)) shifting up '''
+    heap[index] = key
+    while index > 0 and heap[parent(index)] < heap[index]:
+        heap[parent(index)], heap[index] = heap[index], heap[parent(index)]
+        index = parent(index)
+    return heap
+
+
+def heap_decrease_key(heap, index, key):
+    ''' function for decreasing key time-complexity = O(log(n)) shifting down '''
+    heap[index] = key
+    return heapify(heap, index)
+
+
+def max_heap_insert(heap, key):
+    ''' function for inserting key time-complexity = O(log(n)) '''
+    heap.append(float('-inf'))
+    return heap_increase_key(heap, len(heap)-1, key)
+
+
+def max_heap_delete(heap, index):
+    ''' function for deleting key time-complexity = O(log(n)) '''
+    heap[index] = heap[-1]
+    return heapify(heap[:-1], index)
+
+
+def build_max_heap_with_insert():
+    ''' function for building key time-complexity = O(n log(n)) that uses shifting up  '''
+    a = map(int, input("enter numbers to make heap:\t").split())
+    heap = []
+    for item in a:
+        heap = max_heap_insert(heap, item)
+    return heap
+
+
+print(build_max_heap_with_insert())
